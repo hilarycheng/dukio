c1f5b79769fde2e23533e179b282787518ea6e16
Hilary Cheng <hilary@polayer.com>
2015-08-28 07:47:40 +0800

    - Add Skeleton to Construct a Native Object back to JS

--- tests/test-obj.c
+++ tests/test-obj.c
@@ -1,14 +1,25 @@
 #include "duktape.h"
 
+static duk_ret_t do_testobj_callback(duk_context *ctx)
+{
+  printf("%s %d\n", __PRETTY_FUNCTION__, __LINE__);
+
+  return 0;
+}
+
+const duk_function_list_entry test_testobj_callback_funcs[] = {
+    { "callback", do_testobj_callback, 0 },
+    { NULL, NULL, 0 }
+};
+
 static duk_ret_t do_callback(duk_context *ctx)
 {
-  duk_bool_t rc;
-  printf("%s %d Constructor : %d\n", __PRETTY_FUNCTION__, __LINE__, duk_is_constructor_call(ctx));
-//  duk_push_string(ctx, "Constructor from testObj");
-//  duk_put_prop_string(ctx, -1, "Hello");
+  printf("%s %d\n", __PRETTY_FUNCTION__, __LINE__);
 
-printf("stack top is %ld\n", (long) duk_get_top(ctx));
-
+  duk_push_this(ctx);
+  duk_push_number(ctx, 10);
+  duk_put_prop_string(ctx, -2, "hello");
+  duk_put_function_list(ctx, -1, test_testobj_callback_funcs);
 
   return 0;
 }
@@ -24,10 +35,10 @@ int main(int argc, char **argv)
 
   duk_push_global_object(ctx);              // Global
   duk_push_c_function(ctx, do_callback, 0); // Cons
-  duk_put_prop_string(ctx, -2, "testobj"); 
+  duk_put_prop_string(ctx, -2, "testobj");
 //  duk_push_global_object(ctx);
 //  duk_put_function_list(ctx, -1, test_module_funcs);
-//  duk_pop(ctx);
+  duk_pop(ctx);
 
   duk_eval_file(ctx, "test-obj.js");
   duk_destroy_heap(ctx);
--- tests/test-obj.js
+++ tests/test-obj.js
@@ -2,3 +2,6 @@
 var result = new testobj();
 
 print(result);
+print(result.hello);
+print(result.callback);
+result.callback();

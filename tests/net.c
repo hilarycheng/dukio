65724fc0bf28d3c31ad016edf7f3c8e66b45e54d
Hilary Cheng <hilary@polayer.com>
2015-08-29 12:00:02 +0800

    - Add Listen Partial Implementation

--- tests/net.c
+++ tests/net.c
@@ -1,3 +1,4 @@
+#include <uv.h>
 #include "duktape.h"
 
 static int32_t net_next_id = 1;
@@ -7,7 +8,25 @@ static duk_ret_t net_createConnection(duk_context *ctx) {
 }
 
 static duk_ret_t net_serverListen(duk_context *ctx) {
+  uv_tcp_t *tcp;
+  duk_size_t size;
+  struct sockaddr_in bind_addr;
   printf("%s %d\n", __PRETTY_FUNCTION__, __LINE__);
+
+  const char *address = duk_require_string(ctx, 0);
+  duk_int_t port = duk_require_int(ctx, 1);
+
+  printf("Address : %s , Port : %d\n", address, port);
+
+  duk_push_this(ctx);
+  duk_get_prop_string(ctx, -1, "\xff""\xff""uv_tcp");
+  tcp = (uv_tcp_t *) duk_to_fixed_buffer(ctx, -1, &size);
+  duk_pop(ctx);
+
+  bind_addr = uv_ip4_addr(address, port);
+  uv_tcp_bind(tcp, bind_addr);
+  uv_listen((uv_stream_t *) tcp, 128, NULL);
+
   return 0;
 }
 
@@ -34,27 +53,37 @@ const duk_function_list_entry net_server_functions_list[] = {
 };
 
 static duk_ret_t net_createServer(duk_context *ctx) {
+  duk_bool_t rc = 0;
   int32_t timer_id = net_next_id++;
   duk_int_t p0 = duk_get_type(ctx, 0);
   duk_int_t p1 = duk_get_type(ctx, 1);
 
   if (duk_is_object(ctx, 0) && duk_is_callable(ctx, 0)) { // Function
     printf("Function\n");
-    duk_push_this(ctx); // [ this ]
+
+    duk_push_object(ctx); // [ this ]
     duk_push_object(ctx); // [ object object ]
-    duk_put_prop_string(ctx, -2, "\xff""\xff""options"); // [ object.options ]
+    rc = duk_put_prop_string(ctx, -2, "\xff""\xff""options"); // [ object.options ]
+    printf("Options : %d\n", rc);
     duk_dup(ctx, 0); // [ object.options function ]
-    duk_put_prop_string(ctx, -2, "\xff""\xff""callback"); // [ netobject ]
+    rc = duk_put_prop_string(ctx, -2, "\xff""\xff""callback"); // [ netobject ]
+    printf("Callback : %d\n", rc);
     duk_put_function_list(ctx, -1, net_server_functions_list);
     duk_push_c_function(ctx, net_serverFinalizer, 1);
     duk_set_finalizer(ctx, -2);
 
+    uv_tcp_t *tcp = (uv_tcp_t *) duk_push_fixed_buffer(ctx, sizeof(uv_tcp_t));
+    uv_tcp_init(uv_default_loop(), tcp);
+    rc = duk_put_prop_string(ctx, -2, "\xff""\xff""uv_tcp");
+    printf("UVTCP: %d\n", rc);
+
+  printf("UV TCP Pointer %p\n", tcp);
     return 1;
   } else if (duk_is_object(ctx, 0) && !duk_is_callable(ctx, 0) &&
     duk_is_object(ctx, 1) && duk_is_callable(ctx, 1)) { // Object , Function
     printf("Function & Obj\n");
 
-    duk_push_this(ctx); // [ object ]
+    duk_push_object(ctx); // [ object ]
     duk_dup(ctx, 0); // [ object object ]
     duk_put_prop_string(ctx, -2, "\xff""\xff""options"); // [ object.options ]
     duk_dup(ctx, 1); // [ object.options function ]
@@ -62,6 +91,11 @@ static duk_ret_t net_createServer(duk_context *ctx) {
     duk_put_function_list(ctx, -1, net_server_functions_list);
     duk_push_c_function(ctx, net_serverFinalizer, 1);
     duk_set_finalizer(ctx, -2);
+
+    uv_tcp_t *tcp = (uv_tcp_t *) duk_push_fixed_buffer(ctx, sizeof(uv_tcp_t));
+    uv_tcp_init(uv_default_loop(), tcp);
+    duk_put_prop_string(ctx, -2, "\xff""\xff""uv_tcp");
+  printf("UV TCP Pointer %p\n", tcp);
 
     return 1;
   } else {
@@ -92,10 +126,13 @@ void net_Init(duk_context *ctx) {
 }
 
 int main(int argc, char **argv) {
+  uv_loop_t *loop = uv_default_loop();
   duk_context *ctx = duk_create_heap_default();
 
   net_Init(ctx);
   duk_eval_file(ctx, "tests/net_test.js");
+
+  uv_run(loop, UV_RUN_DEFAULT);
 
   duk_destroy_heap(ctx);
 
--- tests/net_test.js
+++ tests/net_test.js
@@ -4,6 +4,10 @@ var s0 = net.createServer({ a: 1 }, function(kde) { });
 var s1 = net.createServer(function(abc) { });
 
 print(s1.address());
+s1.listen('0.0.0.0', 7000);
+for (var s in s1) {
+  print('-----' + s);
+}
 
 s0 = null;
 s1 = null;
